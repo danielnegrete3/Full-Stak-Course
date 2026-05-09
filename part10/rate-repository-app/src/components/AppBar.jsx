fdf96c02c7f62d6cc3e3b8951aaf8b447108091d
Daniel Alejandro Negrete Gomez <daniel.negrete@grupomag.com.mx>
2026-05-09 10:54:30 -0600

    exercise(4)

---
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -0,0 +1,40 @@
+import { View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import Text from './Text';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    display:'flex',
+    flexDirection:'row',
+    alignItems:'center',
+    alignContent:'center',
+    gap:3,
+    backgroundColor:'#3d3d3d'
+    // ...
+  },
+  items:{
+    color:'#EEEEEE',
+    fontSize:20,
+    paddingVertical:'auto',
+  }
+  // ...
+});
+
+const pages = {
+    'Repositories':'',
+}
+
+const AppBar = () => {
+  return (
+    <View style={styles.container}>
+        {
+            Object.entries(pages).map(([Name,Page])=>
+                <Text fontWeight='bold'  key={`bar-${Name}`} style={styles.items}>{Name}</Text>
+            )
+        }
+    </View>
+  );
+};
+
+export default AppBar;
