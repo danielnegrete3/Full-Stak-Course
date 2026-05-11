447b209a5d69c04e3a79996d4d0bb53e7148fbf0
Daniel Alejandro Negrete Gomez <daniel.negrete@grupomag.com.mx>
2026-05-11 15:50:27 -0600

    exercise(7)

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,17 +1,24 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import { Link } from 'react-router-native';
 import Text from './Text';
 
 const styles = StyleSheet.create({
+  scroller: {
+    width:'100%',
+    backgroundColor:'#3d3d3d',
+    padding:0,
+    margin:0,
+    height:'10%',
+    maxHeight:'10%'
+  },
   container: {
-    paddingTop: Constants.statusBarHeight,
+    paddingTop: '4%',
     display:'flex',
     flexDirection:'row',
     alignItems:'center',
     justifyContent:'start',
     gap:10,
-    backgroundColor:'#3d3d3d',
     // ...
   },
   items:{
@@ -28,13 +35,16 @@ const pages = {
 
 const AppBar = () => {
   return (
-    <View style={styles.container}>
+    
+    <ScrollView horizontal style={styles.scroller}>
+        <View style={styles.container}>
         {
             Object.entries(pages).map(([Name,Page])=>
                 <Link to={Page} key={`bar-${Name}`} ><Text fontWeight='bold' style={styles.items}>{Name}</Text></Link>
             )
         }
-    </View>
+      </View>
+    </ScrollView>
   );
 };
 
